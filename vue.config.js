e7b319a79b0033bc22b80ff2d6be4e9fcfd60932
personal <k01midy@gmail.com>
2023-12-14 23:34:12 +0800

    ooo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "https://kamiiliu.github.io/R2B1-mobile/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "https://kamiiliu.github.io/r2b1-mobile/" : "/",
   transpileDependencies: true
 })
