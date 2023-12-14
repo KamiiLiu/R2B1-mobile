39da575c4da33f9efa520c5a1efc22b434a9d980
Kaamii <kami.yh@medialand.tw>
2023-12-14 19:19:12 +0800

    feat

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,59 @@
-<template lang="pug">
-p aaa
+<template>
+  <div>
+    <div v-if="items.length === 0">
+      陣列為空的畫面
+    </div>
+    <div v-else>
+      陣列有資料的畫面
+      <ul>
+        <p>{{ items }}</p>
+      </ul>
+    </div>
+  </div>
 </template>
   
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-@Options({
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
+import dayjs from '@/utils/dayjs';
+import { getDatabase, ref as firebaseRef, onValue, DataSnapshot } from "firebase/database";
+import firebaseApp from '@/firebase/firebaseConfig';
+export default defineComponent({
+  name: 'GameSetting',
   components: {
-    // GameSetting
+  },
+  setup() {
+    const items = ref([]);
+    const date = dayjs.tz(new Date()).format('YYYY-MM-DD')
+    const database = getDatabase(firebaseApp);
+    const listRef = firebaseRef(database, date);
+    let unsubscribe: () => void;
+    onMounted(() => {
+      unsubscribe = onValue(listRef, (snapshot: DataSnapshot) => {
+        const data = snapshot.val();
+        console.log(snapshot.val())
+        if (data) {
+          items.value = data;
+        } else {
+          items.value = [];
+        }
+      });
+    });
+
+    onUnmounted(() => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    });
+    return{
+      items
+    }
   }
 })
-export default class HomeView extends Vue {}
 </script>
 <style scoped>
-.home{
+.home {
   width: 100vw;
   height: 100vh;
 }
 </style>
-  
+
